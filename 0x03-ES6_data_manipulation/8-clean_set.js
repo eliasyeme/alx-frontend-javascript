9ec0e7a6291dc87f36d86a28a136b0421b5ecaa7
eliasyeme <eliasyeme@gmail.com>
2024-04-16 04:16:42 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (!set || !startString) {
+    return '';
+  }
+
   return [...set].filter((value) => value.startsWith(startString)).map((value) => value.slice(startString.length)).join('-');
 }
